c763d3431c92ae2c1431ab55edd08ff8d0d9f4c1
TheSheepGuy <TheSheepGuy1@gmail.com>
2020-01-18 17:10:14 +0000

    Upgraded to .NET 3.1 and elaborated dependencies.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace WikipediaViewer
+namespace wikipediaviewer
 {
     class Program
     {
